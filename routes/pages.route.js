adfc1f039dd0f8bbad189f8546c1e1246edebc38
dzhao37 <danielzhao04@gmail.com>
2023-10-06 16:57:23 -0400

    Frontend
    Wrote index.ejs file to create the skeleton of the page. Wrote index.css file to control the appearance of the page. Modified the controller file to configure the view engine to ejs and added route to pages.route.js.

--- routes/pages.route.js
+++ routes/pages.route.js
@@ -2,12 +2,16 @@ import express from 'express'
 import path from 'path'
 import UserController from "../controllers/user.controller.js";
 
-const router  = express.Router()
+const router = express.Router()
 
 router.route("/").get(UserController.getAllUsers);
 
 router.route("/page2").get((req, res) => {
     res.sendFile(path.resolve() + '/page2.html')
 })
+
+router.route("/css/index.css").get((req, res) => {
+    res.sendFile(path.resolve() + "/css/index.css");
+});
 
 export default router
