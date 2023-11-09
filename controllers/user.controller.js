d7b3ffe1d677dc59217056cd12838948bfcc9c0a
dzhao37 <danielzhao04@gmail.com>
2023-11-08 22:59:59 -0500

    Authentication and Authorization
    Added functionality for verifying that the user is who they claim to be as well as access to the profile once they log in.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,71 +1,84 @@
 import UserAccessor from "../db_accessor/user.accessor.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import Auth from "../auth/authorization.js";
 
 export default class UserController {
-    static async getAllUsers(req, res) {
-        const users = await UserAccessor.getAllUsers();
-        console.log(users);
-        res.render("index", { users: users } );
-    }
-    
+  static async getAllUsers(req, res) {
+    const users = await UserAccessor.getAllUsers();
+    res.render("index", { users: users } );
+  }
+  
+  static getLoginPage(req, res) {
+    res.render("login_page", { error: req.cookies.error });
+  }
 
-    static getLoginPage(req, res) {
-        res.render("login_page", { error: req.cookies.error });
+  static getSignUpPage(req, res) {
+    res.render("sign_up");
+  }
+  
+  static getProfile(req, res, next) {
+    if (!req.error) {
+      const user = Auth.getUserInfo(req);
+      res.render("profile", {
+        name: user.username,
+        email: user.email,
+        bio: user.bio,
+        followers: user.followers,
+        following: user.following,
+      });
+    } else {
+      return next();
     }
+  }
 
-    static getSignUpPage(req, res) {
-        res.render("sign_up");
-    }
-
-
-    static async postSignUp(req, res, next) {
-        try {
-            req.body.password = await bcrypt.hash(req.body.password, 10);
-            await UserAccessor.createUser(req.body);
-            res.redirect("/login-page");
-        } catch (e) {
-            res.error = 999;
-            next();
-        }
-    }
-    
-    static async postLogin(req, res, next) {
+  static async postSignUp(req, res, next) {
       try {
-        if(!req.cookies.token) {
-          const user = await UserAccessor.getAllUsers(req.body.username);
-          if(user) {
-            const result = await bcrypt.compare(req.body.password, user.password);
-            if(result) {
-              const token = jwt.sign(
-                {
-                  username: user.username,
-                  email: user.email,
-                  bio: user.bio,
-                  followers: user.followers,
-                  following: user.following,
-                },
-                process.env.TOKEN_KEY
-              );
-              res.cookie("token", token, {
-                httpOnly: true,
-                maxAge: 60 * 60 * 1000,
-              });
-              res.redirect("/profile");
-            } else {
-              req.error = 400;
-              next();
-            }
+          req.body.password = await bcrypt.hash(req.body.password, 10);
+          await UserAccessor.createUser(req.body);
+          res.redirect("/login-page");
+      } catch (e) {
+          res.error = 999;
+          next();
+      }
+  }
+  
+  static async postLogin(req, res, next) {
+    try {
+      if(!req.cookies.token) {
+        const user = await UserAccessor.getUser(req.body.username);
+        if(user) {
+          const result = await bcrypt.compare(req.body.password, user.password);
+          if(result) {
+            const token = jwt.sign(
+              {
+                username: user.username,
+                email: user.email,
+                bio: user.bio,
+                followers: user.followers,
+                following: user.following,
+              },
+              process.env.TOKEN_KEY
+            );
+            res.cookie("token", token, {
+              httpOnly: true,
+              maxAge: 60 * 60 * 1000,
+            });
+            res.redirect("/profile");
           } else {
             req.error = 400;
             next();
           }
         } else {
-          res.redirect("/profile");
+          req.error = 400;
+          next();
         }
-      } catch (e) {
-        req.error = 400;
-        next();
+      } else {
+        res.redirect("/profile");
       }
+    } catch (e) {
+      req.error = 400;
+      next();
     }
+  }
 }
